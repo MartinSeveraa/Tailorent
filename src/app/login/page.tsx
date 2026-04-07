b0d44a0b9606c794fa1e26241171a3a4a99b4719
MartinSevera <st75788@upce.cz>
2026-04-07 12:27:35 +0200

    login fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 // src/app/login/page.tsx
 import { Suspense } from "react";
-import LoginForm from "./LoginForm";
+import LoginForm from "./loginForm";
 
 export default function LoginPage() {
   return (
